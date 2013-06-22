e73b72d6f211ae5c15ba0ab52b3fc0305e76b7cd
yangbo1979 <gyangbo@gmail.com>
2013-06-22 15:11:38 +0900

    Update LeweiTcpClient.ino

--- examples/LeweiTcpClient/LeweiTcpClient.ino
+++ examples/LeweiTcpClient/LeweiTcpClient.ino
@@ -1,105 +1,44 @@
-#include <SPI.h>
-#include <Ethernet.h>
 #include <LeweiTcpClient.h>
-byte mac[] = {0x74,0x69,0x69,0x2D,0x30,0x31};
 
-#define LW_USERKEY "yourapikey"
+#define LW_USERKEY "your api key"
 #define LW_GATEWAY "01"
+//long starttime;
+//int postInterval = 60000;
 
-IPAddress ip(192,168,1, 15);
-IPAddress mydns(8,8,8,8);
-IPAddress gw(192,168,1,1);
-IPAddress subnet(255,255,255,0);
- 
 LeweiTcpClient *client;
+
 void setup()
 {
-  Serial.begin(9600);
-  //you can use 3 ways to init your network
-  //1.simplest
+  Serial.begin(115200);//we recommended using 38400 bps(need to set the same as your wifi shield)
+  //starttime = millis();
   client = new LeweiTcpClient(LW_USERKEY, LW_GATEWAY);
-  //2.set mac only
-  //client = new LeweiTcpClient(LW_USERKEY, LW_GATEWAY,mac);
-  //3.full setting for your network
-  //client = new LeweiTcpClient(LW_USERKEY, LW_GATEWAY,mac,ip,mydns,gw,subnet);
-  
-  
-  //test1 is the function you write below to handle your program
-  //"testFunction" is the function name you set on website:http://www.lewei50.com/
-  //on "controll command manager" menu
-  //we test transfer 1-5 para from website(you defined on it) to your arduino
-  //enjoy it
-  
-	UserFunction uf1(test1,"testFunction");
+  UserFunction uf1(test1,"testFunction");
   client->addUserFunction(uf1);
   UserFunction uf2 (test2,"testFunction2");
   client->addUserFunction(uf2);
-//  UserFunction uf3 (test3,"testFunction3");
-//  client->addUserFunction(uf3);
-//  UserFunction uf4 (test4,"testFunction4");
-//  client->addUserFunction(uf4);
-//  UserFunction uf5 (test5,"testFunction5");
-//  client->addUserFunction(uf5);
-  UserFunction uf6 (test6,"getAllSensors");
-  client->addUserFunction(uf6);
-  
 }
 
 void loop()
-{
-    client->keepOnline();
-}
+{ 
+client->keepOnline();
+    
+//    if ((millis()-starttime) > postInterval)
+//    {
+//      String val = "1000";
+//      //client->sendSensorValue("dsm501",val);
+//      starttime = millis();
+//    }
+ }
 
-
-//function test1 use 1 parameter,on the website,it point to "p1"
 void test1(char * p1)
 {
-  client->setRevCtrlMsg("true","message to server");
-  Serial.println("test function recall1");
-//  double pi = 3241.59;//*random(10);
-//  client->sendSensorValue("tcp1",pi);
-  Serial.println(p1);
+  client->setRevCtrlMsg("true","message to myServer");
+  //client->sendSensorValue("dsm501","100");
 }
 //function test2 use 2 parameter,on the website,it point to "p1,p2"
 void test2(char * p1,char * p2)
 {
-  Serial.println("test function recall2");
   client->setRevCtrlMsg("true","finished function2");
-  Serial.println(p1);
-//  client->sendSensorValue("tcp1",301);
-  Serial.println(p2);
-}
-void test3(char * p1,char * p2,char * p3)
-{
-  Serial.println("test function recall3");
-  Serial.println(p1);
-  Serial.println(p2);
-  Serial.println(p3);
-}
-void test4(char * p1,char * p2,char * p3,char * p4)
-{
-  Serial.println("test function recall4");
-  Serial.println(p1);
-  Serial.println(p2);
-  Serial.println(p3);
-  Serial.println(p4);
-}
-void test5(char * p1,char * p2,char * p3,char * p4,char * p5)
-{
-  Serial.println("test function recall5");
-  Serial.println(p1);
-  Serial.println(p2);
-  Serial.println(p3);
-  Serial.println(p4);
-  Serial.println(p5);
+  //client->sendSensorValue("dsm501",101.23);
 }
 
-void test6()
-{
-  Serial.println("test function recall6");
-  
-  //client->directResponse("{\"method\":\"response\",\"result\":{\"successful\":true,\"message\":null,\"data\":[{\"id\":\"1\",\"type\":\"jdq\",\"name\":\"s1\",\"value\":\"1\",\"status\":\"ok\"},{\"id\":\"2\",\"type\":\"jdq\",\"name\":\"s2\",\"value\":\"1\",\"status\":\"ok\"}]}}");
-  //String msg = "{\"successful\":true,\"message\":null,\"data\":[{\"id\":\"1\",\"type\":\"jdq\",\"name\":\"s1\",\"value\":\"1\",\"status\":\"ok\"},{\"id\":\"2\",\"type\":\"jdq\",\"name\":\"s2\",\"value\":\"1\",\"status\":\"ok\"},{\"id\":\"3\",\"type\":\"jdq\",\"name\":\"s3\",\"value\":\"0\",\"status\":\"ok\"}]}";
-  char* msg ="test";
-  client->setRevCtrlMsg("true",msg);
-}
