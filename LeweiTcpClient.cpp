34c99b814f787874c3c087fe6263ab9d6c76e86b
yangbo1979 <gyangbo@gmail.com>
2013-07-08 11:26:04 +0800

    add sendsensorValue long type

--- LeweiTcpClient.cpp
+++ LeweiTcpClient.cpp
@@ -279,6 +279,11 @@ void LeweiTcpClient::sendSensorValue(String sensorName,double sensorValue)
 	sendSensorValue(sensorName,(String)s);
 	
 }
+void LeweiTcpClient::sendSensorValue(String sensorName,long sensorValue)
+{
+	sendSensorValue(sensorName,(String)sensorValue);
+	
+}
 
 
 void LeweiTcpClient::execute(void (*callfuct)())
